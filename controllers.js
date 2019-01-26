e347499a9a90ac1839bbcb4c36c506520d4ee94a
Glitch (skitter-candle) <none>
2019-01-26 15:35:11 +0000

    wip create new user controller

--- controllers.js
+++ controllers.js
@@ -1,4 +1,5 @@
 // Controllers for different routes
+const shortid = require('shortid');
 
 // send default test message
 exports.test = (req, res) => {
@@ -7,7 +8,28 @@ exports.test = (req, res) => {
 
 // Create a new user
 exports.createUser = (req, res) => {
-
+  // post a username
+  // return object with username and id (from shortid);
+  // {
+  //   "username": "tony",
+  //   "_id": "SyDl2g5XN"
+  // }
+  
+  // get username
+  const username = req.body.username;
+  
+  // generate shortid
+  const newShortId = shortid.generate();
+  
+  // make new document for it
+  
+  // save to db
+  
+  // send response
+  return res.json({
+    username,
+    _id: newShortId
+  });
 };
 
 // Add an exercise to a given user
