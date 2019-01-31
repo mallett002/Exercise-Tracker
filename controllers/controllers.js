7dd5b3668593b35fa3dfe68eed2405d9b74a501c
William Mallett <mallett002@gmail.com>
2019-01-31 07:21:07 -0600

    fixing create-user to handle existing usernames

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,5 @@
-// Controllers for different routes
 const shortid = require('shortid');
+const User = require('../models/user.model');
 
 // send default test message
 exports.test = (req, res) => {
@@ -20,24 +20,62 @@ exports.createUser = (req, res) => {
   const userModel = require('../models/user.model');
   const userDoc = new userModel({
     username,
-    _id: newShortId
+    id: newShortId
   });
-  
-  // save it and send response
-  userDoc.save()
+
+  // check if user already exists
+  User.findOne({username: username})
     .then(data => {
-      res.json(data);
+      if (data !== null) {
+        return res.send({message: 'Username already exists'});
+      } 
+
+      // save it and send response
+      userDoc.save()
+      .then(() => {
+        return res.json({
+          username,
+          id: newShortId
+        });
+      });
     })
     .catch(err => {
-      res.status(500).send({
-        message: err.message || "Something broke while creating the product"
-      });
+      return res.send({message: err.message});
     });
 };
 
 // Add an exercise to a given user
 exports.addExercise = (req, res) => {
+  const userId = req.body.userId;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date = req.body.date;
 
+  if (!userId || !description || !duration) {
+    return res.status(400).send('Fields marked with * are required');
+  }
+
+  const newExercise = {
+    userId,
+    description,
+    duration,
+    date: date && date // if date is true then use date
+  };
+
+  User.findOneAndUpdate({id: userId}, {$push: {logs: newExercise}}, {new: true})
+    .then(user => {
+      if (!user) {
+          return res.status(404).send({
+              message: `User not found with id ${userId}`
+          });
+      }
+      return res.json(newExercise);
+    })
+    .catch(err => {
+      return res.status(500).send({
+        message: `Something went wrong updating product with id ${userId}`
+      });
+    });
 };
 
 // Get exercise log for user
--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use('/api/exercise', user);
 // Serve static files and serve HTML
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log('Your app is listening on port ' + listener.address().port);
 });
